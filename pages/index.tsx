41da78bd00d70473d10c2eb08617afc4072d9d95
Kipras Januska <kiprasjanuska0@gmail.com>
2023-06-11 17:32:10 -0400

    Add pronunciation tooltip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,54 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import { useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Kipras Januska</title>
-        <meta name="description" content="Kipras' personal website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const [pronunciationShown, setPronunciationShown] = useState(false);
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hi, I'm Kipras
-        </h1>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Kipras Januska</title>
+				<meta name="description" content="Kipras' personal website" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-        <p className={styles.description}>
-          Welcome to my personal website
-        </p>
+			<main className={styles.main}>
+				<div>
+					{pronunciationShown && <div id={styles.pronunciation}>Pronounced "Kip-res Jah-new-ska"</div>}
+					<h1>
+						Hi, I'm{" "}
+						<span id={styles.name} onMouseEnter={() => setPronunciationShown(true)} onMouseLeave={() => setPronunciationShown(false)}>
+							Kipras Januska
+						</span>
+					</h1>
+				</div>
 
-        <h2>
-          About me
-        </h2>
-        
-        <h2>
-          Projects
-        </h2>
-      </main>
+				<p>Welcome to my personal website</p>
 
-      <footer className={styles.footer}>
-        <a
-          href="mailto:kjanuska@umich.edu"
-        >
-          kjanuska@umich.edu
-        </a>
-      </footer>
-    </div>
-  )
-}
+				<h2>About me</h2>
+				<p>I am an undergraduate Computer Science student at the University of Michigan, Ann Arbor.</p>
+				<h3>Things I enjoy</h3>
+				<ul>
+					<li>Cooking</li>
+					<li>Discovering new music genres</li>
+					<li>Lifting weights</li>
+					<li>Playing guitar</li>
+				</ul>
+				<h2>Projects</h2>
+				<ul>
+					<li>CourseKit</li>
+					<li></li>
+				</ul>
+			</main>
 
-export default Home
+			<footer className={styles.footer}>
+				<a href="mailto:kjanuska@umich.edu">kjanuska@umich.edu</a>
+			</footer>
+		</div>
+	);
+};
+
+export default Home;
