a847fda08deef6a0c480ca0a7288bd6adc16ed4a
Kipras Januska <kiprasjanuska0@gmail.com>
2024-12-26 14:53:10 -0600

    Update hobby images to hand drawn ones

--- pages/components/Hobby.tsx
+++ pages/components/Hobby.tsx
@@ -2,18 +2,12 @@ import styles from "./Hobby.module.css";
 
 const Hobby = ({ hobby }: any) => {
 	return (
-		<div className={`${styles.hobby} card`}>
+		<div className={styles.hobby}>
+            <img className={styles.image} src={hobby.image} alt={hobby.alt} />
             <div className={styles.info}>
                 <p className={styles.title}>{hobby.title}</p>
                 <p className={styles.description}>{hobby.description}</p>
-                {hobby.social && (
-                    <a className={styles.social} target="_blank" href={hobby.social.link}>
-                        <img className={styles.icon} src={hobby.social.icon} alt={hobby.social.alt}/>
-                        <span>{hobby.social.text}</span>
-                    </a>
-                )}
             </div>
-			<img className={styles.gif} src={hobby.image} alt={hobby.alt} />
 		</div>
 	);
 };
